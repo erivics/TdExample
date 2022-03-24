bc6f0fefd98c11b9b99663fa3660a61fe28e72af
Adesina Kolawole Bayo <75021908+erivics@users.noreply.github.com>
2022-03-24 13:30:57 +0100

    initial

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using TdExample.Example;
+
+namespace TdExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////Console.WriteLine("Hello World!");
+            //Triangle triangle = new Triangle(7.6 , 7.4);
+            //triangle.AcceptDetails();
+            //triangle.CalculateArea();
+            //triangle.DisplayResult();
+            //Console.ReadLine();
+        }
+    }
+}
