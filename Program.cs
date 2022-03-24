c83fc15589244642d49b71e1f70c3f84f6feac0c
Adesina Kolawole Bayo <75021908+erivics@users.noreply.github.com>
2022-03-24 15:59:08 +0100

    switch case

--- Program.cs
+++ Program.cs
@@ -7,12 +7,32 @@ namespace TdExample
     {
         static void Main(string[] args)
         {
-            ////Console.WriteLine("Hello World!");
-            //Triangle triangle = new Triangle(7.6 , 7.4);
-            //triangle.AcceptDetails();
-            //triangle.CalculateArea();
-            //triangle.DisplayResult();
-            //Console.ReadLine();
+           
+            SwitchGradeExample myGrade = new SwitchGradeExample();
+            myGrade.AddGrades(91);
+            myGrade.AddGrades(86.7f);
+            myGrade.AddGrades(69);
+
+            
+            //Am using this method to pass myGrade into GradeStatistic object and ofcourse selecting the highest, Lowest and avaerage grade
+            GradeStatistic stats = myGrade.ComputeStatistics();
+            WriteResult("Average", stats.AverageGrade);
+            WriteResult("Highest", stats.HighestGrade);
+            WriteResult("Lowers", stats.LowestGrade);
+            WriteResult(stats.Description, stats.LetterGrade);
+        }
+
+        
+        
+        //I want to kind of modify the WriteLine a bit with this method. Not a big deal guys
+        static void WriteResult(string description, float result)
+        {
+            Console.WriteLine($"{description} : {result}" , description, result);
+        }
+
+        static void WriteResult(string description, string result)
+        {
+            Console.WriteLine($"{description} : {result}", description, result);
         }
     }
 }
